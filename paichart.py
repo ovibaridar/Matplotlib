b0290ec893d52e901e03846d42b45f068c869e1c
ovibaridar <ovibaridar06@gmail.com>
2023-10-16 01:03:15 +0600

    bar chart

--- paichart.py
+++ paichart.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
+
 result_gread = ['F', 'D', 'C', 'B', 'A-', 'A', 'A+']
 result_value = [10, 9, 20, 40, 50, 10, 5]
-plt.pie(result_value, labels=result_gread, autopct='%2.1f%%', explode=[0.1,.1 ,0.1, 0.1, .1, 0.1, 0.1])
+plt.pie(result_value, labels=result_gread, autopct='%2.2f%%', explode=[0.1, .1, 0.1, 0.1, .1, 0.1, 0.1])
 plt.show()
